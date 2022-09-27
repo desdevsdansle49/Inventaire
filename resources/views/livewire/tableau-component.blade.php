a9f18189102ed3ee0c2171a2f45ca71835a1af60
CyriaqueSoden <73218277+CyriaqueSoden@users.noreply.github.com>
2022-09-27 20:25:22 +0200

    loading

--- resources/views/livewire/tableau-component.blade.php
+++ resources/views/livewire/tableau-component.blade.php
@@ -25,66 +25,70 @@
 
 
                         {{-- form --}}
-                        @if ($fromEdit)
-                            <form wire:submit.prevent="remove">
-                            @else
-                                <form wire:submit.prevent="addItem">
-                        @endif
-                        <div class="mb-3">
-                            <label class="form-label">Name</label>
-                            <input wire:model="name" type="text" class="form-control">
-                        </div>
-                        @error('name')
-                            <div class="alert alert-danger p-2" role="alert">
-                                {{ $message }}
-                            </div>
-                        @enderror
-                        <div class="mb-3">
-                            <label class="form-label">Quantity</label>
-                            <input wire:model="quantity" type="text" class="form-control">
-                        </div>
-                        @error('quantity')
-                            <div class="alert alert-danger p-2" role="alert">
-                                {{ $message }}
-                            </div>
-                        @enderror
-                        <label class="form-label">Catégorie</label>
-                        @if ($inputCategory == false)
-                            <div>
-                                <select wire:model="category" class="custom-select ">
-                                    @foreach ($categories as $category)
-                                        <option value="{{ $category->name }}">{{ $category->name }}</option>
-                                    @endforeach
-                                </select>
-                                <button wire:click="showInput" type="button" class="btn btn-secondary">+</button>
-                                <button wire:click="removeCategory" type="button" class="btn btn-secondary">-</button>
-                            </div>
-                        @else
-                            <div>
-                                <div class="mb-3  border rounded p-3">
-                                    <label class="form-label">Catégorie</label>
-                                    <input wire:model="category" type="text" class="form-control mb-2">
-                                    <button wire:click="showInput" type="button"
-                                        class="btn btn-secondary">Annuler</button>
-                                    <button wire:click="addCategory" type="button"
-                                        class="btn btn-primary">Ajouter</button>
-
-                                </div>
-                            </div>
-                        @endif
-
-                        <div align="right">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <div wire:loading.remove>
                             @if ($fromEdit)
-                                <button wire:click="remove" class="btn btn-danger"
-                                    data-bs-dismiss="modal">supprimer</button>
-                                <button type="submite" class="btn btn-primary">Save changes</button>
-                            @else
-                                <button type="submit" class="btn btn-primary">Ajouter</button>
+                                <form wire:submit.prevent="remove">
+                                @else
+                                    <form wire:submit.prevent="addItem">
                             @endif
-                        </div>
+                            <div class="mb-3">
+                                <label class="form-label">Name</label>
+                                <input wire:model="name" type="text" class="form-control">
+                            </div>
+                            @error('name')
+                                <div class="alert alert-danger p-2" role="alert">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                            <div class="mb-3">
+                                <label class="form-label">Quantity</label>
+                                <input wire:model="quantity" type="text" class="form-control">
+                            </div>
+                            @error('quantity')
+                                <div class="alert alert-danger p-2" role="alert">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                            <label class="form-label">Catégorie</label>
+                            @if ($inputCategory == false)
+                                <div>
+                                    <select wire:model="category" class="custom-select ">
+                                        @foreach ($categories as $category)
+                                            <option value="{{ $category->name }}">{{ $category->name }}</option>
+                                        @endforeach
+                                    </select>
+                                    <button wire:click="showInput" type="button" class="btn btn-secondary">+</button>
+                                    <button wire:click="removeCategory" type="button"
+                                        class="btn btn-secondary">-</button>
+                                </div>
+                            @else
+                                <div>
+                                    <div class="mb-3  border rounded p-3">
+                                        <label class="form-label">Catégorie</label>
+                                        <input wire:model="category" type="text" class="form-control mb-2">
+                                        <button wire:click="showInput" type="button"
+                                            class="btn btn-secondary">Annuler</button>
+                                        <button wire:click="addCategory" type="button"
+                                            class="btn btn-primary">Ajouter</button>
 
-                        </form>
+                                    </div>
+                                </div>
+                            @endif
+
+
+                            <div align="right">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                @if ($fromEdit)
+                                    <button wire:click="remove" class="btn btn-danger"
+                                        data-bs-dismiss="modal">supprimer</button>
+                                    <button type="submite" class="btn btn-primary">Save changes</button>
+                                @else
+                                    <button type="submit" class="btn btn-primary">Ajouter</button>
+                                @endif
+                            </div>
+
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
