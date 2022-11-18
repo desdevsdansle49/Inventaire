a2ef1e58aeae72450efdf3dcc21420bf43a10485
LunarDreams <43464800+lunar-d@users.noreply.github.com>
2022-11-18 16:45:03 +0100

    Logo SVG

--- resources/views/livewire/navigation-menu.blade.php
+++ resources/views/livewire/navigation-menu.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">
-            <img src="{{ asset('logo-police.png') }}" alt="Police Nationale" height="50" />
+            <img src="{{ asset('img/logo-police.svg') }}" alt="Police Nationale" height="50" />
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
